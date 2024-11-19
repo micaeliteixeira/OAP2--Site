881443dd87780a80df689bb875ce52e3e8b723ae
Micaeli <luime6#gmail.com>
2024-11-19 20:41:27 -0300

    feat: footer

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import './styled.css'
+
+import Facebook from '../../../assets/facebook.svg'
+import Instagram from '../../../assets/instagram.svg'
+import Logo from '../../../assets/logo.svg'
+import Twitter from '../../../assets/twitter.svg'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className="footer-container">
+      <div className="footer-container-values">
+        <div className="footer-container-values-top">
+          <img src={Logo} alt="Logo da página" width={'100px'} />
+          <p className="p-subtitle">Everyone can be a chef in their own kitchen</p>
+        </div>
+
+        <div className="footer-container-values-icons">
+          <div className="footer-container-values-icon">
+            <img src={Facebook} alt="Icone do facebook" />
+            <img src={Twitter} alt="Icone do twitter" />
+            <img src={Instagram} alt="Icone do instagram" />
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
